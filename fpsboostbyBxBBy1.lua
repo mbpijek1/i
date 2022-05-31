19303c3ff1bee65508aaa0eed54b1e5808dd3100
mbpijek1 <96808780+mbpijek1@users.noreply.github.com>
2022-05-31 11:26:42 +0100

    Create fpsboostbyBxBBy1.lua

---
+++ fpsboostbyBxBBy1.lua
@@ -0,0 +1,37 @@
+
+    cc.Parent = game.Lighting
+    cc.Saturation = -0.1
+    cc.Contrast = -0.1
+    lighting.GlobalShadows = false
+
+    sethiddenproperty(lighting, "Technology", Enum.Technology.Compatibility) 
+local g = game
+local w = g.Workspace
+local l = g.Lighting
+local t = w.Terrain
+
+for i, v in pairs(g:GetDescendants()) do
+    if v:IsA("Part") or v:IsA("Union") or v:IsA("CornerWedgePart") or v:IsA("TrussPart") then
+        v.Material = "Plastic"
+        v.Reflectance = 0
+    elseif v:IsA("ParticleEmitter") or v:IsA("Trail") then
+        v.Lifetime = NumberRange.new(0)
+    elseif v:IsA("Explosion") then
+        v.BlastPressure = 1
+        v.BlastRadius = 1
+    elseif v:IsA("Fire") or v:IsA("SpotLight") or v:IsA("Smoke") or v:IsA("Sparkles") then
+        v.Enabled = false
+    end
+end
+for i, e in pairs(l:GetChildren()) do
+    if e:IsA("BlurEffect") or e:IsA("SunRaysEffect") or e:IsA("ColorCorrectionEffect") or e:IsA("BloomEffect") or e:IsA("DepthOfFieldEffect") then
+        e.Enabled = false
+    end
+end
+
+sethiddenproperty(t,"Decoration",false)
+t.WaterWaveSize = 0
+t.WaterWaveSpeed = 0
+t.WaterReflectance = 0
+t.WaterTransparency = 0
+l.FogEnd = 9e9
